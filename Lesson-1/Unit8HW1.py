7adc7afa70f7c513bec421a937972cdbead6fa38
Elijah_abel <149628259+Axiom08@users.noreply.github.com>
2024-04-17 13:44:22 -0700

    ok

--- Lesson-1/Unit8HW1.py
+++ Lesson-1/Unit8HW1.py
@@ -1,13 +1,42 @@
 '''
-Name:
-Date:
-Assignment:
+Name: Elijah Abel
+Date: 4/15/2024
+Assignment: unit 8 homework 1
 '''
 
 # Vehicle class here
+class Vehicle:
+    def __init__(self, num_wheels:int, num_occupants:int, color:str = 'black'):
+        self.num_wheels = num_wheels
+        self.num_occupants = num_occupants
+        self.color = color
+        self.max_occupants = 5
+
+    def occupant_increase(self, num_of_additional_occupants):
+        try:
+            self.num_occupants += num_of_additional_occupants
+            if self.num_occupants > self.max_occupants:
+                raise ValueError
+            else:
+                return self.num_occupants
+        except ValueError:
+            print(f'You can have no more than {self.max_occupants} people in the vehicle.')
+            self.num_occupants -= num_of_additional_occupants
+    
 
 def main():
-    print()
+    # question 2
+    vehicle_1 = Vehicle(2, 1, 'red')
+    vehicle_2 = Vehicle(18, 3, 'green')
+    print(f'Vehicle one is carrying {vehicle_1.num_occupants} occupant.')
+    print(f'Vehicle two is the color {vehicle_2.color}.')
+
+    # question 3
+    print(vehicle_1.num_occupants)
+    vehicle_1.occupant_increase(6)
+    print(vehicle_1.num_occupants)
+
+
 
 if __name__ == '__main__':
     main()
--- Lesson-1/main.py
+++ Lesson-1/main.py
@@ -1,7 +1,25 @@
 # Point class goes here
+import math
+class Point:
+    # constructor
+    def __init__(self, x_val:int, y_val:int):
+        self.x = x_val
+        self.y = y_val
+        
+
+    def distance(self, second_point):
+        leg_a = self.x - second_point.x
+        leg_b = self.y - second_point.y
+        distance = math.sqrt(leg_a**2 + leg_b**2)
+        return distance
+
 
 def main():
-    print()
+    origin = Point(0, 0)
+    c = Point(-3, 4)
+    print(f'The origin is ({origin.x}, {origin.y})')
+    print(f'The distance between {(origin.x, origin.y)} and {(c.x, c.y)} is {origin.distance(c)}')
+
 
 if __name__ == '__main__':
     main()
